3ccca17c79d06e381b7a01c18e20443dc112650a
robertschimpfle <robert.schimpfle@comquent.de>
2017-07-17 20:05:24 +0200

    Update cucumberJava.java

--- src/main/java/cucumberJava/cucumberJava.java
+++ src/main/java/cucumberJava/cucumberJava.java
@@ -1,7 +1,7 @@
 package cucumberJava; 
 import org.openqa.selenium.By; 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
